79582a6ba5572ab92f9ffeb3e58c126def53c475
Sniper_Monkey <114159904+RomeuTB@users.noreply.github.com>
2022-09-22 10:35:28 -0300

    Add files via upload
    por favor use este código

---
+++ lolo.py
@@ -0,0 +1,8 @@
+while 1 == 1:
+    pergunta = float(input("Insira o tamanho do seu pênis: "))
+    meuPau = 32.6
+
+    if pergunta < meuPau:
+        print("Ganhei")
+
+    else: print("Esse é o meu garoto")
